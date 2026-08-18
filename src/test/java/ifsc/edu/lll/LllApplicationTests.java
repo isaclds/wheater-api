bcac7365d8dc71f99ca96d2959723e90b73feb6c
Isac Lehmkuhl dos Santos <isacldossan@gmail.com>
2026-08-18 19:18:41 -0300

    feat: configuração inicial do projeto

---
+++ src/test/java/ifsc/edu/lll/LllApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsc.edu.lll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
