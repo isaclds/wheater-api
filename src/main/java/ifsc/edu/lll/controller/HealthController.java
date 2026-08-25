d5cc6dde635032a71a76280e248e2c508b28a734
Bernardo Lisboa <bernardo.al2006@aluno.ifsc.edu.br>
2026-08-25 11:08:43 -0300

    fix(controller): arrumando o HealthController para retornar a ApiResponse

--- src/main/java/ifsc/edu/lll/controller/HealthController.java
+++ src/main/java/ifsc/edu/lll/controller/HealthController.java
@@ -1,5 +1,6 @@
 package ifsc.edu.lll.controller;
 
+import ifsc.edu.lll.dto.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/ping")
-    public ResponseEntity<String> ping() {
-        return ResponseEntity.ok("API funcionando.");
+    public ResponseEntity<ApiResponse<String>> ping() {
+        return ResponseEntity.ok(ApiResponse.sucess("OK"));
     }
 }
